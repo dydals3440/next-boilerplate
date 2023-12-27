748603f85cd8dc6db60324d60c67537b4627ded0
Matthew <dydals3440@gmail.com>
2023-12-27 23:33:36 +0900

    feat(app/page.tsx): 로그인 버그 수정 h3태그 추가
    로그인 버그 수정을 했습니다. 1번 기능이 어디서 문제가발생해서, 2번 기능 개발해서 도입했습니다.

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,8 @@ import React from 'react';
 export default function Home() {
   return (
     <>
-      <h3>로그인 버그 다 수정함 힘들었다.</h3>
+      <h3>로그인 버그 다 수정함 힘들었다.123123</h3>
+      <h3>로그인 버그 다 수정함 힘들었다.123123</h3>
     </>
   );
 }
